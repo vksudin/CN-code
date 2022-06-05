6bdc9912d3b62954a3476440a4100d58e2285e98
Vedshruti-11 <68157517+Vedshruti-11@users.noreply.github.com>
2022-06-05 08:46:34 +0530

    Update CRC.c

--- CRC.c
+++ CRC.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
-char data[20],div[20],temp[4],total[100];
+#include<string.h>
+char data[20],div[20],temp[40],total[100];
 int i,j,datalen,divlen,len,flag=1;
 void check();
 int main()
 {
- 	printf("Enter the total bit of data:");    
- 	scanf("%d",&datalen);
- 	printf("\nEnter the total bit of divisor");
- 	scanf("%d",&divlen);
- 	len=datalen+divlen-1;	
  	printf("\nEnter the data:");
- 	scanf("%s",&data);		      
-    printf("\nEnter the divisor");
+ 	scanf("%s",data);		      
+    printf("\nEnter the divisor:");
     scanf("%s",div); 
-      
+   datalen=strlen(data);
+   divlen=strlen(div);
+   len=datalen+divlen-1;
 	for(i=0;i<datalen;i++)
 	{
 		total[i]=data[i]; 
@@ -24,8 +22,8 @@ int main()
 	check();								
  	for(i=0;i<divlen;i++)	 			
 	  	temp[i+datalen]=data[i];
- 	printf("\ntransmitted Code Word:%s",temp);	  	
-	printf("\n\nEnter the received code word:");
+ 	printf("\ntransmitted Code Word: %s",temp);	  	
+	printf("\n\nEnter the received code word: ");
     scanf("%s",total); 
  	check();		 	  
 	for(i=0;i<divlen-1;i++)
